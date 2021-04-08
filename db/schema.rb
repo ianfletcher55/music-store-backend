65a6c620fbfe8d09ada166261eac0721cb25295e
Erik Alvar <erikalvarbass@gmail.com>
2021-04-08 16:55:07 -0400

    adds all order crud actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_07_153116) do
+ActiveRecord::Schema.define(version: 2021_04_08_201940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_04_07_153116) do
     t.decimal "total", precision: 10, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
   create_table "product_categories", force: :cascade do |t|
