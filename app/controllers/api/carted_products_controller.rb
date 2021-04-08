175eb00df30fa8349b85fe6f56dcf176c8983c5e
Erik Alvar <erikalvarbass@gmail.com>
2021-04-08 15:50:33 -0400

    changes to single quotes and adds carted_products create action

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,6 +1,21 @@
 class Api::CartedProductsController < ApplicationController
   def index
     @carted_products = CartedProduct.all
-    render "index.json.jb"
+    render 'index.json.jb'
+  end
+
+  def create
+    @carted_product = CartedProduct.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      order_id: params[:order_id],
+      quantity: params[:quantity],
+      status: params[:status],
+    )
+    if @carted_product.save
+      render 'show.json.jb'
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,17 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/products" => "products#index"
-    get "/products/:id" => "products#show"
-    post "/products" => "products#create"
-    patch "/products/:id" => "products#update"
-    delete "/products/:id" => "products#destroy"
+    get '/products' => 'products#index'
+    get '/products/:id' => 'products#show'
+    post '/products' => 'products#create'
+    patch '/products/:id' => 'products#update'
+    delete '/products/:id' => 'products#destroy'
 
-    get "/carted_products" => "carted_products#index"
+    get '/carted_products' => 'carted_products#index'
+    post '/carted_products' => 'carted_products#create'
 
-    post "/users" => "users#create"
+    post '/users' => 'users#create'
 
-    post "/sessions" => "sessions#create"
+    post '/sessions' => 'sessions#create'
   end
 end
