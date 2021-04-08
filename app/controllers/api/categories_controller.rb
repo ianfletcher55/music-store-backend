6bd968db23c82725492eb508e63e40aa61913544
ianfletcher55 <ian.fletcher55@gmail.com>
2021-04-08 15:10:44 -0500

    fixes error with products partial, adds index, create, and destroy actions to categories, and adds view and routes for categories

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,2 +1,25 @@
 class Api::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+    render 'index.json.jb'
+  end
+
+  def create
+    @category = Category.new(
+      name: params[:name]
+    )
+    if @category.save
+      render 'index.json.jb'
+    else
+      render json: {errors: @category.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    @category.destroy
+    render json: { message: 'Category successfully destroyed' }
+  end
+
 end
