4257203944401171a432f650321c87c95de2f6ca
ianfletcher55 <ian.fletcher55@gmail.com>
2021-04-12 09:43:12 -0500

    adds migration file to rename product_categories' product_id column to correct singular form

--- app/controllers/api/product_categories_controller.rb
+++ app/controllers/api/product_categories_controller.rb
@@ -1,2 +1,20 @@
 class Api::ProductCategoriesController < ApplicationController
+
+  def create
+    @category = Category.new(
+      name: params[:name]
+    )
+    if @category.save
+      render 'index.json.jb'
+    else
+      render json: {errors: @category.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    @category.destroy
+    render json: { message: 'Category successfully destroyed' }
+  end
+
 end
---
+++ db/migrate/20210412144143_rename_product_id_column.rb
@@ -0,0 +1,4 @@
+class RenameProductIdColumn < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
